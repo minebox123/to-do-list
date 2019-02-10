cdf0d769e2ff8daae8decaee82943e39d4ba3490
minebox123 <solodeev.09@mail.com>
2019-02-10 07:02:37 +0300

    Upload the newest version

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,12 +1,24 @@
-// How to use it button
+// Variables:
 
-function inf() {
-  document.getElementById('rule1').innerHTML = '1.   Keep it short, and prioritaze daily.';
-  document.getElementById('rule2').innerHTML = '2.   Separate home and work (and school, and kids, and everything else).';
-  document.getElementById('rule3').innerHTML = '3.   Keep it accessible. All the time.';
-  document.getElementById('rule4').innerHTML = '4.   Identify and separate distracting ideas from actual tasks.';
-  document.getElementById('rule5').innerHTML = '5.   Do the most important things first.';
-};
+let deleteAll = document.createElement('span'); //  trash button
+const addButton = document.querySelector('#btn');
+let storageArray = localStorage.getItem('newTask') ? JSON.parse(localStorage.getItem('newTask')) : [];
+const data = JSON.parse(localStorage.getItem('newTask'));
+
+
+// 'How to use it' button
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+
+  const using = document.querySelector('.how_to_use_it')
+  if (using.style.display === 'none') {
+    using.style.display = 'flex';
+  } else {
+    using.style.display = 'none';
+  }
+}, false);
+
 
 
 // Enter button
@@ -18,12 +30,14 @@ document.body.onkeyup = function(enter) {
 };
 
 
-// Create a close button and append it to each list item
+
+// A to-do list
 
 function addNewElement() {
   let newItem = document.createElement('LI');
   let userInp = document.getElementById('userInput').value;
   let textNode = document.createTextNode(userInp);
+
   newItem.appendChild(textNode);
   if (userInp == '') {
     return alert('Please, type something!');
@@ -32,21 +46,102 @@ function addNewElement() {
   }
   document.getElementById('userInput').value = '';
 
+  // Setting up a localStorage.
+
+  localStorage.setItem('newTask', JSON.stringify(storageArray));
+  storageArray.push(userInp);
+  localStorage.setItem('newTask', JSON.stringify(storageArray));
+
   // A close symbol
 
-  let span = document.createElement('span');
+  let closeSymbol = document.createElement('span');
   let symbol = document.createTextNode('\u00D7');
-  span.className = 'close';
-  span.appendChild(symbol);
-  newItem.appendChild(span);
+  closeSymbol.className = 'close';
+  closeSymbol.appendChild(symbol);
+  newItem.appendChild(closeSymbol);
+
+  // An input checkbox object
+
+  let labCont = document.createElement('label'); // a container that contains input and span.
+  labCont.className = 'container';
+  let checkbox = document.createElement('input'); // a checkbox input.
+  checkbox.setAttribute('type', 'checkbox');
+  let sp = document.createElement('span'); // a span tag.
+  sp.className = 'checkmark';
+  labCont.appendChild(checkbox);
+  labCont.appendChild(sp);
+
+  textNode.before(labCont); // Adding the labCont before textNode.
 
   // Deleting the current item
 
-  span.addEventListener('click', function(e) {
+  closeSymbol.addEventListener('click', function() {
     newItem.parentNode.removeChild(newItem);
+    localStorage.removeItem('newTask');
   }, false);
 
+  // making a line-through/none text decoration.
+
+  checkbox.addEventListener('click', function() {
+    if (checkbox.checked == true) {
+      newItem.style.textDecoration = 'line-through';
+    } else if (checkbox.checked == false) {
+      newItem.style.textDecoration = 'none';
+    }
+  });
+
+};
+
+// Saved tasks.
+
+data.forEach(item => {
+
+  let newItem = document.createElement('LI');
+  let userInp = item;
+  let textNode = document.createTextNode(userInp);
+  newItem.appendChild(textNode);
+  document.getElementById('list').appendChild(newItem);
 
 
+  // checkbox.
 
-}
+  let labCont = document.createElement('label'); // a container that contains input and span.
+  labCont.className = 'container';
+  let checkbox = document.createElement('input'); // a checkbox input.
+  checkbox.setAttribute('type', 'checkbox');
+  let sp = document.createElement('span'); // a span tag.
+  sp.className = 'checkmark';
+  labCont.appendChild(checkbox);
+  labCont.appendChild(sp);
+
+  textNode.before(labCont); // Adding the labCont before textNode.
+
+  checkbox.addEventListener('click', function() {
+    if (checkbox.checked == true) {
+      newItem.style.textDecoration = 'line-through';
+    } else if (checkbox.checked == false) {
+      newItem.style.textDecoration = 'none';
+    }
+  });
+
+  deleteAll.addEventListener('click', event => {
+    localStorage.clear();
+    newItem.parentNode.removeChild(newItem);
+  })
+
+});
+
+// the 'delete all' button
+
+let buttonName = document.createTextNode('');
+deleteAll.appendChild(buttonName);
+deleteAll.className = 'deleteAll';
+let user = document.getElementById('userInput');
+user.before(deleteAll);
+user.style.marginRight = '30px';
+addButton.style.position = 'relative';
+addButton.style.right = '20px'
+
+//eleteAll.addEventListener('click', function() {
+//  localStorage.clear();
+//});
